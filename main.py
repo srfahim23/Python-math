de46e397acc0d125fda83ac916b767f65e6f331b
Muhammad Faysal <srfahim23@gmail.com>
2023-02-12 11:55:11 +0600

    This-update

---
+++ main.py
@@ -0,0 +1,38 @@
+# Python Math "|| The min() and max() functions used 
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+#The abs() function returns the absulate (positive) value of the specified number:
+x = abs(-7.25)
+print(x)
+
+#Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+x = pow(4, 3)
+print(x)
+
+#The Math Module
+#import math
+
+# The math.sqrt() method for example, returns the square root of a number:
+import math 
+
+x = math.sqrt(64)
+print(x)
+
+#The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downards to its nearest integer, and returns the result:
+import math
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)  # returns 2
+print(y)  # reutns 1
+
+# The math.pi constant, returns the value of PI (3.14...):
+import math
+x = math.pi
+
+print(x)
